04d6b5fb7d35553ea26685b81efdb4b52b785292
Allen Tsai <hello.allen.tsai@gmail.com>
2020-01-27 22:05:42 -0800

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'app.js',
+    path: __dirname + '/public'
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
